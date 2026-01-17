1454605a791159956c5278a3edf1bf547055bcf0
sirbikandras <sirbik.andras@gmail.com>
2026-01-17 16:25:05 +0100

    Refactored project structure and added routing with React Router. Removed unused CSS and assets, updated `vite.config.ts` for custom server port, and implemented a new `CounterView` component.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {createBrowserRouter, NavLink, RouterProvider} from "react-router";
+import CounterView from "./view/CounterView.tsx";
+
+function Root() {
+    return <div>
+        <NavLink to="/counter">Counter</NavLink>
+    </div>
+}
+
+const router = createBrowserRouter([
+    {path: "/", Component: Root},
+    {path: "/counter", Component: CounterView},
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return <RouterProvider router={router}/>
 }
 
 export default App
---
+++ frontend/src/view/CounterView.tsx
@@ -0,0 +1,34 @@
+import {useState} from "react";
+import viteLogo from "../assets/vite.svg";
+import reactLogo from "../assets/react.svg";
+import {NavLink} from "react-router";
+
+export default function CounterView() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <NavLink to="/">Home</NavLink>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo"/>
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    )
+}
