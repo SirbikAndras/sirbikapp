dfd0052ffe1eda4069efdc9b87e607452d342853
sirbikandras <sirbik.andras@gmail.com>
2026-01-17 15:57:22 +0100

    Initial commit

---
+++ src/main/java/com/sirbik/sirbikapp/SirbikappApplication.java
@@ -0,0 +1,13 @@
+package com.sirbik.sirbikapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirbikappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SirbikappApplication.class, args);
+    }
+
+}
